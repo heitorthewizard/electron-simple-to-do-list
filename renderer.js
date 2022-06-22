e9937114198d9452d62d478b82f8c2d29d55d585
heitorthewizard <heitor.mb27@gmail.com>
2022-06-21 21:08:50 -0300

    turned my simple to do list app into a desktop application

--- renderer.js
+++ renderer.js
@@ -1,6 +1,64 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const list = document.querySelector("#list");
+const inputTask = document.querySelector("#taskInput");
+const addButton = document.querySelector("#addButton");
+let listOfAddedTasks = [];
+
+// add storage tasks to the list of array
+if (listOfAddedTasks.length == 0) {
+  const storageTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  listOfAddedTasks.push(...storageTasks);
+}
+
+buildListOfTasks();
+
+addButton.addEventListener("click", addTaskToListOfTasks);
+
+function addTaskToListOfTasks() {
+  if (inputTask.value.length != 0) {
+    listOfAddedTasks.push(inputTask.value);
+
+    // add task to local storage
+    localStorage.setItem('tasks', JSON.stringify(listOfAddedTasks))
+
+    buildListOfTasks();
+
+    // clear input
+    inputTask.value = "";
+  }
+}
+
+function buildListOfTasks() {
+  list.innerHTML = "";
+  for (let i = 0; i < listOfAddedTasks.length; i++) {
+    const item = document.createElement("li");
+    const task = document.createElement("span");
+    const removeButton = document.createElement("button");
+
+    item.id = "id" + i;
+    task.innerText = listOfAddedTasks[i];
+    removeButton.innerText = "X";
+    removeButton.setAttribute("onclick", `removeTaskFromList(${i})`);
+
+    item.appendChild(task);
+    item.appendChild(removeButton);
+    list.appendChild(item);
+  }
+}
+
+function removeTaskFromList(id) {
+  const itemToRemove = document.querySelector("#id" + id);
+  list.removeChild(itemToRemove);
+
+  // remove the task from the array
+  const firstHalf = listOfAddedTasks.slice(0, id);
+  const secondHalf = listOfAddedTasks.slice(id + 1);
+  listOfAddedTasks = firstHalf.concat(secondHalf);
+
+  // update list of tasks
+  buildListOfTasks();
+
+  // update local storage
+  localStorage.setItem("tasks", JSON.stringify(listOfAddedTasks));
+}
+
+//localStorage.clear()
